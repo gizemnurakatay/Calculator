5ed81d394c5c2eccdd08dc0e489b83cc9bee22df
gizembkurt <gizem.bkurt>
2022-11-01 21:12:36 +0300

    CalculatorSwift

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     var result = 0
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var myTextField2: UITextField!
+    @IBOutlet weak var myTextField1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func subButton(_ sender: Any) {
+        if let firstNumber = Int(myTextField1.text!){
+            if let secondNumber = Int(myTextField2.text!){
+                result = firstNumber - secondNumber
+                
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func sumButton(_ sender: Any) {
+        if let firstNumber = Int(myTextField1.text!){
+            if let secondNumber = Int(myTextField2.text!){
+                result = firstNumber + secondNumber
+                
+                resultLabel.text = String(result)}}
+    }
+    @IBAction func divButton(_ sender: Any) {
+        if let firstNumber = Int(myTextField1.text!){
+            if let secondNumber = Int(myTextField2.text!){
+                result = firstNumber / secondNumber
+                
+                resultLabel.text = String(result)
+                
+            }
+        }
+    }
+    @IBAction func multbutton(_ sender: Any) {
+        if let firstNumber = Int(myTextField1.text!){
+            if let secondNumber = Int(myTextField2.text!){
+                result = firstNumber * secondNumber
+                
+                resultLabel.text = String(result)}}
+    }
 }
 
